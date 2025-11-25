1302cfd24a63ea2a6b70ffcd5cbfcf438888a9d1
nehagupta150288-lab <nehagupta150288@gmail.com>
2025-11-26 01:53:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// SLIDER
+let index = 0;
+const slides = document.querySelectorAll(".slider img");
+
+function showSlide() {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+}
+
+document.getElementById("rightArrow").onclick = () => {
+    index = (index + 1) % slides.length;
+    showSlide();
+};
+
+document.getElementById("leftArrow").onclick = () => {
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide();
+};
+
+// POPUP IMAGE VIEWER
+function openFull(img) {
+    document.getElementById("popup-img").src = img;
+    document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
