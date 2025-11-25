4969087e3e44879fadaabab525e04c39d1e35544
nehagupta150288-lab <nehagupta150288@gmail.com>
2025-11-26 02:27:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-// SLIDER
+// SLIDER CODE
 let index = 0;
 const slides = document.querySelectorAll(".slider img");
 
-function updateSlide() {
+function showSlide() {
     slides.forEach(s => s.classList.remove("active"));
     slides[index].classList.add("active");
 }
 
 document.getElementById("rightArrow").onclick = () => {
     index = (index + 1) % slides.length;
-    updateSlide();
+    showSlide();
 };
 
 document.getElementById("leftArrow").onclick = () => {
     index = (index - 1 + slides.length) % slides.length;
-    updateSlide();
+    showSlide();
 };
 
-// POPUP IMAGE
+// POPUP IMAGE VIEWER
 function openFull(img) {
     document.getElementById("popup-img").src = img;
     document.getElementById("popup").style.display = "flex";
