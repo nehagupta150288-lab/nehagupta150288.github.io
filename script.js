5f1ca9d60911b7923ef04c5f477d90266fca69d4
nehagupta150288-lab <nehagupta150288@gmail.com>
2025-11-26 08:05:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-// SLIDER CODE
-let index = 0;
-const slides = document.querySelectorAll(".slider img");
+window.onload = function() {
 
-function showSlide() {
-    slides.forEach(s => s.classList.remove("active"));
-    slides[index].classList.add("active");
-}
+    // SLIDER
+    let index = 0;
+    const slides = document.querySelectorAll(".slider img");
 
-document.getElementById("rightArrow").onclick = () => {
-    index = (index + 1) % slides.length;
-    showSlide();
+    function showSlide() {
+        slides.forEach(s => s.classList.remove("active"));
+        slides[index].classList.add("active");
+    }
+
+    document.getElementById("rightArrow").onclick = () => {
+        index = (index + 1) % slides.length;
+        showSlide();
+    };
+
+    document.getElementById("leftArrow").onclick = () => {
+        index = (index - 1 + slides.length) % slides.length;
+        showSlide();
+    };
+
+    // GALLERY POPUP
+    window.openFull = function(img) {
+        document.getElementById("popup-img").src = img;
+        document.getElementById("popup").style.display = "flex";
+    }
+
+    window.closePopup = function() {
+        document.getElementById("popup").style.display = "none";
+    }
 };
-
-document.getElementById("leftArrow").onclick = () => {
-    index = (index - 1 + slides.length) % slides.length;
-    showSlide();
-};
-
-// POPUP IMAGE VIEWER
-function openFull(img) {
-    document.getElementById("popup-img").src = img;
-    document.getElementById("popup").style.display = "flex";
-}
-
-function closePopup() {
-    document.getElementById("popup").style.display = "none";
-}
