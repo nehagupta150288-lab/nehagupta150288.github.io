a2dea0c2ed47eddcb60ffdbe064167391ddb2a51
nehagupta150288-lab <nehagupta150288@gmail.com>
2025-11-26 08:48:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-window.onload = function() {
+window.onload = function () {
 
-    // SLIDER
     let index = 0;
     const slides = document.querySelectorAll(".slider img");
+    const dots = document.querySelectorAll(".indicators span");
 
     function showSlide() {
-        slides.forEach(s => s.classList.remove("active"));
+        slides.forEach((s, i) => {
+            s.classList.remove("active");
+            dots[i].classList.remove("active-dot");
+        });
+
         slides[index].classList.add("active");
+        dots[index].classList.add("active-dot");
     }
 
+    // Arrows
     document.getElementById("rightArrow").onclick = () => {
         index = (index + 1) % slides.length;
         showSlide();
@@ -19,7 +25,13 @@ window.onload = function() {
         showSlide();
     };
 
-    // GALLERY POPUP
+    // Dot clicking
+    window.goToSlide = function(i){
+        index = i;
+        showSlide();
+    }
+
+    // Gallery popup
     window.openFull = function(img) {
         document.getElementById("popup-img").src = img;
         document.getElementById("popup").style.display = "flex";
